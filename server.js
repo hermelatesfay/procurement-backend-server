b9834f5924d41720f4287094b177cd408918aaf5
hermelatesfay <hermelatesfay802@gmail.com>
2026-02-05 04:28:13 +0300

    made changes

--- server.js
+++ server.js
@@ -3,70 +3,60 @@ const fs =require("fs")
 
 const server = http.createServer((req,res) =>{
 //using GET request
-if(req.method ==="GET"){
-    fs.readFile("data.json",(err,data)=>{
-         if (err) {
-                res.writeHead(404, {
-                    "Content-Type": "application/json"
-                })
-                res.end()
-        }else{
-            
-
-            res.writeHead(200, {
-                            "Content-Type": "application/json"
-            })
-
-            res.write(JSON.stringify(JSON.parse(data)))
-            res.end()
-        }
-    })
-}else if (req.method ==="POST"){
-    let body =""
-    req.on("data",(chunk)=>{
-        body += chunk.toString()
-    })
-    req.on("end",()=>{
-        body = JSON.parse(body)
-        console.log(body)
-    })
-    fs.readFile("data.json",(err,data)=>{
-        if (err) {
-                res.writeHead(404, {
-                    "Content-Type": "application/json"
-                })
-                res.end()
-        }else{
-            let newData =JSON.parse(data)
-            newData.push(body)
-            let procurementString = JSON.stringify(newData)
-            fs.writeFile("data.json",procurementString,(err)=>{
+     if(req.url === "/kgl/procurement" && req.method === "GET"){
+            fs.readFile("./data.json",(err,data)=>{
                 if(err){
-                     res.writeHead(500, {
-                        "content-type": "application/json"
-                    })
-        
-                        res.write(JSON.stringify({ message: err }))
-                        res.end()
-                    } else {
-
-                        res.writeHead(201, {
-                            "content-type": "application/json"
-                        })
-
-                        res.write(JSON.stringify({
-                            message: "Procurement added successfully",
-                            body
-                        }))
-
-                        res.end()
-
-
+                    res.writeHead(400,{"Content-Type":"application/json"});
+                    res.end(JSON.stringify({error:"Invalid error", err}));
+                }else{
+                    let dataObject = JSON.parse(data);
+                    console.log(dataObject)
+                    res.writeHead(200,{"Content-Type":"application/json"});
+                    res.end(JSON.stringify({message:"Successful", dataObject}));
                 }
             })
- }
- })
-}
+        }else if(req.url === "/kgl/procurement" && req.method === "POST"){
+            let body = ''
+            req.on("data",(chunck)=>{
+                body += chunck.toString();
+            })
+    
+            req.on("end",()=>{
+                try{
+                    fs.readFile("./data.json",(err,data)=>{
+                        if(err){
+                            res.writeHead(400,{"Content-Type":"application.js"});
+                            res.end(JSON.stringify({error:"Error"}));
+                            
+                        }else{
+                            let newData = JSON.parse(data);
+                             newData.push(body);
+                            let stringified = JSON.stringify(newData);
+                            console.log(stringified)
+    
+                            fs.writeFile("./data.json",stringified,(err)=>{
+                                if(err){
+                                    res.writeHead(400,{"Content-Type":"application.js"});
+                                    res.end(JSON.stringify({error:"Error"}));
+                                }else{
+                                    res.writeHead(201,{"Content-Type":"application.js"});
+                                    res.end(JSON.stringify({message:"Successfully saved",body}));
+                                }
+                            })
+    
+                        }
+                    })
+                    
+                }catch(err){
+                    console.log(err);
+                }
+            })
+        }
+        else{
+            res.writeHead(404);
+            res.end("Page not found");
+        }
+    
 } )
 
 server.listen(3000,()=>{
